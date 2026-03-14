a9385b54f64ed581f9c09ff799236fe646c6fb84
Chris Holden <holdenc137@gmail.com>
2026-03-14 15:09:30 +0000

    replaced some Barrow links with pages

--- src/file-processor.ts
+++ src/file-processor.ts
@@ -110,7 +110,7 @@ export const processFile = async (
             console.log("wikidata-link");
             incrementNamedCounter("wikidata-entity-links");
 
-            if (!fileName.includes("barrow")) {
+            if (!fileName.includes("barrowfffff")) {
               const lastSlash = link.lastIndexOf("/");
               if (lastSlash !== -1) {
                 const q_id = link.substring(lastSlash + 1);
